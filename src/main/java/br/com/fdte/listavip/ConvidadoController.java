3626145609ece0a13a0b4f3642f06f5f28697b59
Bruno Jesus <brnscar10@gmail.com>
2019-02-22 16:16:34 -0300

    Fdte dia 22/2, Springboot

---
+++ src/main/java/br/com/fdte/listavip/ConvidadoController.java
@@ -0,0 +1,50 @@
+package br.com.fdte.listavip;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.fdte.listavip.model.Convidado;
+import br.com.fdte.listavip.repository.ConvidadoRepository;
+
+@Controller
+public class ConvidadoController {
+
+	@Autowired
+	private ConvidadoRepository repository;
+	
+	
+	@RequestMapping ("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping ("listaconvidados")
+	public String listaConvidados (Model model) {
+
+		Iterable<Convidado> convidados = repository.findAll();
+
+		model.addAttribute("convidados", convidados);
+
+		return "listaconvidados";
+	}
+	@RequestMapping (value = "salvar", method = RequestMethod.POST)
+	public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email,
+			@RequestParam("telefone") String telefone, Model model) {
+
+		Convidado novoConvidado = new Convidado(nome, email, telefone);
+		
+		repository.save(novoConvidado); 
+		
+		Iterable<Convidado> convidados = repository.findAll();
+
+		model.addAttribute("convidados", convidados);
+		
+		
+		
+		return "listaconvidados";
+	}
+}
