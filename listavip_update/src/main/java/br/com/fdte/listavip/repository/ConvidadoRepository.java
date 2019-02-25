0b91126c87eb0e9a94f790a6dee4470d14e16f10
Bruno Jesus <brnscar10@gmail.com>
2019-02-25 15:19:21 -0300

    lista vip

---
+++ listavip_update/src/main/java/br/com/fdte/listavip/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fdte.listavip.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.fdte.listavip.model.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long> {
+
+}
